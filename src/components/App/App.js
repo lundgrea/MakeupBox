9c1b3091d52a20ac4ce54bc085f9ceb0bb66c09a
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-11 22:24:44 -0600

    Fetch all makeup  including error and isLoading

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css'
+import { fetchAllMakeup } from '../../apiCalls/apiCalls'
+import { connect } from 'react-redux'
+import { isLoading, handleErrors } from "../../actions";
+
 import PropTypes from 'prop-types'
 import { Route, NavLink } from 'react-router-dom'
 
@@ -9,22 +13,43 @@ export class App extends Component {
   constructor () {
     super()
     this.state = {
-      
+      makeup: []
     }
   }
 
-
-  componentDidMount() {
+  
+  
+  componentDidMount = async () => {
+    try {
+      const allMakeup = await fetchAllMakeup();
+      this.props.isLoading(false)
+      this.setState({makeup: allMakeup})
+    } catch ({message}) {
+      this.props.handleErrors(message)
+      this.props.isLoading(false)
+    }
   }
 
   render() {
+    console.log(this.props)
     return (
       <main className="main">
-      <h1>MAKEUP BOX</h1>
+        <h1>MAKEUP BOX</h1>
+        {this.props.error && <p className="error">{this.props.error}</p>}
+        {!!this.props.isLoading && <p className="loading">Page Is Loading</p>}
       </main>
-      )
+    )
   }
-
 }
 
-export default App;
+export const mapStateToProps = store => ({
+  error: store.hasErrored,
+  isLoading: store.isLoading
+})
+
+export const mapDispatchToProps = dispatch => ({
+  isLoading: bool => dispatch(isLoading(bool)),
+  handleErrors: error => dispatch(handleErrors(error))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
