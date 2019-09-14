58778e512cf5f3c1caf80d0c335f58010ae4e98d
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-14 13:57:32 -0600

    Test all reducers

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Nav from './Nav';
 
+let wrapper;
+
+beforeEach(()=> {
+  wrapper = shallow(<Nav />);
+})
+
 describe('Nav', () => {
   it('should match the snapshot', () => {
-    const wrapper = shallow(<Nav />);
     expect(wrapper).toMatchSnapshot();
   });
 });
--- src/components/QuizQuestion/QuizQuestion.test.js
+++ src/components/QuizQuestion/QuizQuestion.test.js
@@ -13,4 +13,19 @@ describe('QuizQuestion', () => {
     />)
     expect(wrapper).toMatchSnapshot();
   });
-});
+
+  it('should call the handleChange props with the input value when clicked', () => {
+    const handleChangeMock = jest.fn();
+    const mockEvent = {target: {value: 'lips'}}
+    const wrapper = shallow(<QuizQuestion 
+      question='What is your primary interest?'
+      answers={['lips', 'eyes']}
+      key={1}
+      name='interests'
+      handleChange={handleChangeMock}
+     />)  
+
+     wrapper.find('input').at(0).simulate('change', mockEvent)
+     expect(handleChangeMock).toHaveBeenCalledWith(mockEvent)
+  })
+}); 
