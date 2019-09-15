eaed501a5a788f06a726ca4cabc8cbfcae728d10
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-15 16:54:34 -0600

    Integrate redux mapDispatch testing into Nav

--- src/containers/Nav/Nav.test.js
+++ src/containers/Nav/Nav.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Nav } from './Nav';
+import { Nav, mapDispatchToProps } from './Nav';
+import { clearResults, clearResponses } from '../../actions'
 
 let wrapper;
 
@@ -12,4 +13,24 @@ describe('Nav', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+});
+
+describe('mapDispatchToProps', () => {
+  it('should call dispatch with a clearResults action', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = clearResults();
+    const mappedProps = mapDispatchToProps(mockDispatch)
+    mappedProps.clearResults()
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  });
+
+  it('should call dispatch with a clearResponses action', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = clearResponses();
+    const mappedProps = mapDispatchToProps(mockDispatch)
+    mappedProps.clearResponses()
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+
+  });
+
 });
