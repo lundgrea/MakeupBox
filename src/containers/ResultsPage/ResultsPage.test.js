06bbafa0ad3e77c20f2f1501c91a41cd53c137c7
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-15 16:04:05 -0600

    Add mapState and mapDispatch testing to ResultsPage

--- src/containers/ResultsPage/ResultsPage.test.js
+++ src/containers/ResultsPage/ResultsPage.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { ResultsPage } from './ResultsPage';
+import { ResultsPage, mapStateToProps, mapDispatchToProps } from './ResultsPage';
+import {handleErrors, isLoading, getResults} from '../../actions'
+
 
 describe('ResultsPage', () => {
   it('should match the snapshot', () => {
@@ -9,65 +11,74 @@ describe('ResultsPage', () => {
   });
 });
 
-
-
 describe('mapStateToProps', () => {
+  it('should return the quiz responses list ', () => {
+    const mockState = {
+      isLoading: false,
+      results: [[{makeup1: '1'}, {makeup2: '2'}], [{makeup3: '3'}, {makeup4: '4'}], [{makeup5: '5'},{makeup6: '6'}] ],
+      responses: ['lips', 'bold', 'cruelty-free', 'none']
+    }; 
+    const expected = {
+      isLoading: false,
+      results: [[{makeup1: '1'}, {makeup2: '2'}], [{makeup3: '3'}, {makeup4: '4'}], [{makeup5: '5'},{makeup6: '6'}] ],
+      responses: ['lips', 'bold', 'cruelty-free', 'none']
+    };
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
+  });
 
+  it('should return the results from the fetch in an array', () => {
+    const mockState = {
+      isLoading: false,
+      results: [[{makeup1: '1'}, {makeup2: '2'}], [{makeup3: '3'}, {makeup4: '4'}], [{makeup5: '5'},{makeup6: '6'}] ],
+      responses: ['lips', 'bold', 'cruelty-free', 'none']
+    }; 
+    const expected = {
+      isLoading: false,
+      results: [[{makeup1: '1'}, {makeup2: '2'}], [{makeup3: '3'}, {makeup4: '4'}], [{makeup5: '5'},{makeup6: '6'}] ],
+      responses: ['lips', 'bold', 'cruelty-free', 'none']
+    }; 
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
+  });
+
+  it('should return the loading status of the applicaiton', () => {
+    const mockState = {
+      isLoading: false,
+      results: [[{makeup1: '1'}, {makeup2: '2'}], [{makeup3: '3'}, {makeup4: '4'}], [{makeup5: '5'},{makeup6: '6'}] ],
+      responses: ['lips', 'bold', 'cruelty-free', 'none']
+    }; 
+    const expected = {
+      isLoading: false,
+      results: [[{makeup1: '1'}, {makeup2: '2'}], [{makeup3: '3'}, {makeup4: '4'}], [{makeup5: '5'},{makeup6: '6'}] ],
+      responses: ['lips', 'bold', 'cruelty-free', 'none']
+    }; 
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
+  });
 })
 
 describe('mapDispatchToProps', () => {
-  
+  it('should call dispatch with an isLoading action', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = isLoading(false);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.isLoading(false);
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  })
+
+  it('should call dispatch with a handleError action', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = handleErrors('error message');
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.handleErrors('error message');
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  })
+  it('should call dispatch with getResults action', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = getResults([[{makeup1: '1'}, {makeup2: '2'}], [{makeup3: '3'}, {makeup4: '4'}], [{makeup5: '5'},{makeup6: '6'}] ]);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.getResults([[{makeup1: '1'}, {makeup2: '2'}], [{makeup3: '3'}, {makeup4: '4'}], [{makeup5: '5'},{makeup6: '6'}] ]);
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  })
 })
-
-
-
-
-
-// describe("mapStateToProps", () => {
-//   it("should return an object with the albums array", () => {
-//     const mockState = {
-//       albums: [{ album_name: "mirrorland", id: 1 }]
-//     };
-//     const expected = {
-//       albums: [{ album_name: "mirrorland", id: 1 }]
-//     };
-//     const mappedProps = mapStateToProps(mockState);
-
-//     expect(mappedProps).toEqual(expected);
-//   });
-
-//   it("should return an object with a user object", () => {
-//     const mockState = {
-//       user: mockUser
-//     };
-//     const expected = {
-//       user: mockUser
-//     };
-
-//     const mappedProps = mapStateToProps(mockState);
-//     expect(mappedProps).toEqual(expected);
-//   });
-
-//   it("should return an array of favorite album objects", () => {
-//     const mockState = {
-//       favorites: mockAlbums
-//     };
-//     const expected = {
-//       favorites: mockAlbums
-//     };
-//     const mappedProps = mapStateToProps(mockState);
-//     expect(mappedProps).toEqual(expected);
-//   });
-// });
-
-// describe("mapDispatchToProps", () => {
-//   it("calls dispatch with an toggleFavorite action", () => {
-//     const mockDispatch = jest.fn();
-//     const actionToDispatch = toggleFavorite(1);
-//     const mappedProps = mapDispatchToProps(mockDispatch);
-//     mappedProps.toggleFavorite(1);
-
-//     expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
-//   });
-// });
-// });
