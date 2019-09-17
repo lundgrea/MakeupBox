5e21e2f77373f5599baad26c8753a682ae6f5273
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-16 20:50:33 -0600

    Display error message if no quiz results in store

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -3,82 +3,30 @@ import './CardContainer.css';
 import Cards from '../Cards/Cards';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { handleErrors } from '../../actions'
 
 
 export const CardContainer = ({error, categories, data}) => {
 
-
-// const buildCards = () =>{
-//     return data.map(result => {
-//       return result.map((cardInfo, index)=> {
-//         return (
-//           <div key={index}>
-//             <Cards 
-//             {...cardInfo}
-//             key={cardInfo.id}
-//               />
-//           </div>
-//       )
-//     })
-//   })
-// }
-
-
-  // const firstAnswerResults = data[0].slice(0,7);
-  // const secondAnswerResults = data[1].slice(0,7);
-  // const thirdAnswerResults = data[2].slice(0,7);
- 
-  // const firstCards = firstAnswerResults.map((result, index) => {
-  //   return (
-  //       <div key={index}>
-  //         <Cards 
-  //         {...result}
-  //         key={result.id}
-  //         />
-  //       </div>
-  //   )
-  // });
-
-  // const secondCards = secondAnswerResults.map((result, index) => {
-  //   return (
-  //       <div key={index}>
-  //         <Cards 
-  //         {...result}
-  //         key={result.id}
-  //         />
-  //       </div>
-  //     );
-  // });
-
-  // const thirdCards = thirdAnswerResults.map((result, index) => {
-  //   return (
-  //       <div key={index}>
-  //         <Cards 
-  //         {...result}
-  //         key={result.id}
-  //         />
-  //       </div>
-  //     );
-  // });
-
-
   const buildCardDeck = (data) => {
-    return data.map((cardInfo, index)=> {
-      return (
-        <div key={index}>
-          <Cards 
-          {...cardInfo}
-          key={cardInfo.id}
-            />
-        </div>
-      )
-    })
+    if (!data) {
+      return error
+    } else {
+      return data.map((cardInfo, index)=> {
+        return (
+          <div key={index}>
+            <Cards 
+            {...cardInfo}
+            key={cardInfo.id}
+              />
+          </div>
+        )
+      })
+    }
   }
-
     
   return (
     <>
-    {error && <p>{error}</p>}
     <article className='card-container'>
       <section key='0' className='results-category-box'>
         <h2 className='results-category-headline'>{categories[0]}</h2>
@@ -98,14 +46,16 @@ export const CardContainer = ({error, categories, data}) => {
 };
 
 export const mapStateToProps = store => ({
-  error: store.hasErrored,
-  // responses: store.responses,
-  // results: store.results,
+  error: store.hasErrored
 })
 
-export default connect(mapStateToProps)(CardContainer);
+export const mapDispatchToProps = dispatch => ({
+  handleErrors: error => dispatch(handleErrors(error))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardContainer);
 
 CardContainer.propTypes = {
-  data:PropTypes.array.isRequired,
-  // category: PropTypes.string.isRequired  
+  data:PropTypes.array,
+  categories: PropTypes.array 
 };
--- src/containers/ResultsPage/ResultsPage.js
+++ src/containers/ResultsPage/ResultsPage.js
@@ -4,45 +4,30 @@ import { cleanedSearch, cleanedSearchResults } from '../../dataCleaner/dataClean
 import { connect } from 'react-redux';
 import { getResults, isLoading, handleErrors, saveDefaultResponses } from "../../actions";
 import { fetchMakeup } from '../../apiCalls/apiCalls';
-import { CardContainer } from '../../components/CardContainer/CardContainer'
+import { CardContainer } from '../../components/CardContainer/CardContainer';
 
 
 export class ResultsPage extends Component {
  
-  determineSearch = () => { 
-    const defaultSearch = ['lips', 'bold', 'vegan']
-    if (this.props.responses.length < 3) {
-      this.props.saveDefaultResponses(defaultSearch)
-      return this.props.responses
-    } else {
-      return this.props.responses
-    }
-  }
-
-
-
-
-
   async componentDidMount () {
-    const searches = cleanedSearch(this.determineSearch())
+    const searches = cleanedSearch(this.props.responses);
     try {
-      this.props.isLoading(true)
-      const firstResult = await fetchMakeup(searches[0])
-      const cleanedFirstData = await cleanedSearchResults(firstResult)
-      this.props.getResults(cleanedFirstData)
-      const secondResult = await fetchMakeup(searches[1])
-      const cleanedSecondData = await cleanedSearchResults(secondResult)
-      this.props.getResults(cleanedSecondData)
-      const thirdResult = await fetchMakeup(searches[2])
-      const cleanedThirdData = await cleanedSearchResults(thirdResult)
-      this.props.getResults(cleanedThirdData)
+      this.props.isLoading(true);
+      const firstResult = await fetchMakeup(searches[0]);
+      const cleanedFirstData = await cleanedSearchResults(firstResult);
+      this.props.getResults(cleanedFirstData);
+      const secondResult = await fetchMakeup(searches[1]);
+      const cleanedSecondData = await cleanedSearchResults(secondResult);
+      this.props.getResults(cleanedSecondData);
+      const thirdResult = await fetchMakeup(searches[2]);
+      const cleanedThirdData = await cleanedSearchResults(thirdResult);
+      this.props.getResults(cleanedThirdData);
       this.props.isLoading(false);
     } catch ({message}) {
-      this.props.handleErrors(message)
-      this.props.isLoading(false)
-    }
-  }
-
+      this.props.handleErrors(message);
+      this.props.isLoading(false);
+    };
+  };
 
   render() {
     return (
@@ -51,18 +36,18 @@ export class ResultsPage extends Component {
         {this.props.loading && <p className="loading-sentence">Gathering Your Selects</p>}
         {this.props.errors && <p className="loading-sentence">{this.props.errors}</p>}
         {this.props.loading && <div className="loading-div"><img alt='loading' className='loading-animation' src="https://image21.net/103/loading_gif_png/loading_gif_png_59.gif"></img></div>}
-        {!this.props.loading && <CardContainer categories={this.props.responses} data={this.props.results}/>}
+        {!this.props.loading && !this.props.errors && <CardContainer categories={this.props.responses} data={this.props.results}/>}
     </section>
     )
-  }
-}
+  };
+};
 
 export const mapStateToProps = store => ({
   errors: store.hasErrored,
   responses: store.responses,
   results: store.results,
   loading: store.isLoading
-})
+});
 
 export const mapDispatchToProps = dispatch => ({
   isLoading: bool => dispatch(isLoading(bool)),
@@ -71,6 +56,6 @@ export const mapDispatchToProps = dispatch => ({
   saveDefaultResponses: responses => dispatch(saveDefaultResponses(responses))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ResultsPage)
+export default connect(mapStateToProps, mapDispatchToProps)(ResultsPage);
 
 
