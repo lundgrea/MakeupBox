97ad8c029ba67e851f7cefc01e35a0a0490b89be
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-21 18:34:39 -0600

    Refactor duplicative code from Card Container

--- src/containers/ResultsPage/ResultsPage.js
+++ src/containers/ResultsPage/ResultsPage.js
@@ -31,13 +31,21 @@ export class ResultsPage extends Component {
 
   render() {
     return (
-      <section className="results-page">
-        <h3 className="results-page-headline">Your Results</h3>
+      <article className="results-container">
+       <h3 className="results-page-headline">Your Results</h3>
         {this.props.loading && <p className="loading-sentence">Gathering Your Selects</p>}
         {this.props.errors && <p className="loading-sentence">{this.props.errors}</p>}
         {this.props.loading && <div className="loading-div"><img alt='loading' className='loading-animation' src="https://image21.net/103/loading_gif_png/loading_gif_png_59.gif"></img></div>}
-        {!this.props.loading && !this.props.errors && <CardContainer categories={this.props.responses} data={this.props.results}/>}
+      <section className="results-page">
+        {/* <p className="loading-sentence">Gathering Your Selects</p> */}
+
+        {/* <div className="loading-div"><img alt='loading' className='loading-animation' src="https://image21.net/103/loading_gif_png/loading_gif_png_59.gif"></img></div> */}
+
+        {!this.props.loading && !this.props.errors && <CardContainer categories={this.props.responses[0]} data={this.props.results[0]}/>}
+        {!this.props.loading && !this.props.errors && <CardContainer categories={this.props.responses[1]} data={this.props.results[1]}/>}
+        {!this.props.loading && !this.props.errors && <CardContainer categories={this.props.responses[2]} data={this.props.results[2]}/>}
     </section>
+    </article>
     );
   };
 };
